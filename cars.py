3c3bab6478563ccbd82b703911fb16324dc96a98
Natnael Endale Cheru <natnaelendale22@gmail.com>
2025-12-17 21:42:57 +0300

    Add files via upload

---
+++ cars.py
@@ -0,0 +1,37 @@
+import random
+from turtle import Turtle
+
+COLORS = ["red", "orange", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class Car:
+    def __init__(self):
+        self.all_car = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+
+    def create_cars(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_len=2, stretch_wid= 1)
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            new_car.penup()
+            new_car.goto(600, random_y)
+            self.all_car.append(new_car)
+
+    def move(self):
+        for car in self.all_car:
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
+
+
+
+
+
+
